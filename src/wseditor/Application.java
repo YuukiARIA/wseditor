51be46d14a86f3c74f6f7376a5d5092e543d29ce
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-11-22 15:52:08 +0900

    Add class Application

---
+++ src/wseditor/Application.java
@@ -0,0 +1,13 @@
+package wseditor;
+
+public final class Application
+{
+	private static final String VERSION = "0.1.0";
+
+	private Application() { }
+
+	public static String getVersion()
+	{
+		return VERSION;
+	}
+}
