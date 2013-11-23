4c48dc1b36fd99c1908f701ccf92925e7af2126d
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2013-11-23 13:37:40 +0900

    Add an exception

--- src/wseditor/translator/Translator.java
+++ src/wseditor/translator/Translator.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+import wseditor.translator.exception.TranslateException;
 import wseditor.translator.util.WSTree;
 
 public class Translator
@@ -42,43 +43,48 @@ public class Translator
 
 	private char[] cs;
 	private int p;
+	private int line = 1;
+	private int column = 1;
 
 	public Translator(String text)
 	{
 		cs = text.toCharArray();
 	}
 
-	public List<String> translate()
+	public List<String> translate() throws TranslateException
 	{
 		List<String> code = new ArrayList<String>();
+
+		int sline = line, scolumn = column;
 
 		WSTree<Inst> cur = root;
 		while (p < cs.length)
 		{
-			char c = cs[p++];
+			char c = readc();
 			if (!isWS(c)) continue;
 
 			cur = cur.getSubtree(c);
 			if (cur == null)
 			{
-				System.out.println("error");
-				break;
+				throw new TranslateException("illegal command at line " + sline + " column " + scolumn);
 			}
 			else if (cur.isAccept())
 			{
 				gencode(code, cur.getValue());
 				cur = root;
+				sline = line;
+				scolumn = column;
 			}
 		}
 		return code;
 	}
 
-	private void gencode(List<String> code, Inst inst)
+	private void gencode(List<String> code, Inst inst) throws TranslateException
 	{
 		String s = "";
 		if (inst != Inst.LABEL)
 		{
-			s += inst;
+			s += "\t" + inst;
 		}
 		switch (inst)
 		{
@@ -106,23 +112,23 @@ public class Translator
 		code.add(s);
 	}
 
-	private String readToLF()
+	private String readToLF() throws TranslateException
 	{
-		String s = "";
+		StringBuilder buf = new StringBuilder();
 		char c;
-		while ((c = cs[p++]) != '\n')
+		while ((c = readc()) != '\n')
 		{
-			s += (c == ' ' ? '0' : '1');
+			buf.append(c == ' ' ? '0' : '1');
 		}
-		return s;
+		return buf.toString();
 	}
 
-	private BigInteger readNumber()
+	private BigInteger readNumber() throws TranslateException
 	{
 		BigInteger value = BigInteger.ZERO;
 		char c;
-		boolean positive = cs[p++] == ' ';
-		while ((c = cs[p++]) != '\n')
+		boolean positive = readc() == ' ';
+		while ((c = readc()) != '\n')
 		{
 			value = value.shiftLeft(1).or(c == ' ' ? BigInteger.ZERO : BigInteger.ONE);
 		}
@@ -131,6 +137,24 @@ public class Translator
 			value = value.negate();
 		}
 		return value;
+	}
+
+	private char readc() throws TranslateException
+	{
+		if (p < cs.length)
+		{
+			if (cs[p] == '\n')
+			{
+				line++;
+				column = 1;
+			}
+			else
+			{
+				column++;
+			}
+			return cs[p++];
+		}
+		throw new TranslateException("unexpected end");
 	}
 
 	private static boolean isWS(char c)
---
+++ src/wseditor/translator/exception/TranslateException.java
@@ -0,0 +1,10 @@
+package wseditor.translator.exception;
+
+@SuppressWarnings("serial")
+public class TranslateException extends Exception
+{
+	public TranslateException(String message)
+	{
+		super(message);
+	}
+}
